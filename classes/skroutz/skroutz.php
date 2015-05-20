30cc4e4e71e24fae403be55630470c6b49727f89
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-05-20 17:16:20 +0300

    exclude unpublished products

--- classes/skroutz/skroutz.php
+++ classes/skroutz/skroutz.php
@@ -111,7 +111,7 @@ class skroutz extends framework {
 	 * @since 150130
 	 */
 	public function processProducts() {
-		$prodArray = (array) $this->©db->get_col( 'SELECT ID FROM ' . $this->©db->posts . ' WHERE post_type="product"' );
+		$prodArray = (array) $this->©db->get_col( 'SELECT ID FROM ' . $this->©db->posts . ' WHERE post_type="product" AND post_status="publish"' );
 
 		$wpMemLimit = $this->getMemInM(WP_MAX_MEMORY_LIMIT)/1024/1024;
 
